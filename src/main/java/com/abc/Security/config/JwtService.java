44c337e280c283be91b2f4be11938a287ea1ed10
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-10-12 20:36:00 +0200

    complete all methods of jwtServcie

--- src/main/java/com/abc/Security/config/JwtService.java
+++ src/main/java/com/abc/Security/config/JwtService.java
@@ -1,18 +1,26 @@
 package com.abc.Security.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "PqvgBdaDWiwFgeFkBvvRnub0azEr6Ctt";
+    private static final String SECRET_KEY = "6mTKTfMpPtE7MH9Y3WwsdnYfvJgzW1P0";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -21,6 +29,31 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return ExtractExpiration(token).before(new Date());
+    }
+
+    private Date ExtractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
